2d2f1ee2d06d97b8615c93fd8ef0a2586086a83a
si-hyun01 <thcodbs@naver.com>
2024-03-24 19:48:13 +0900

    first commit

---
+++ BalanSe/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className = "black-nav">
+        <div style = { { color : 'blue', fontSize : '30px'} }>개발 블로그</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
